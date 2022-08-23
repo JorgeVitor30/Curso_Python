3fcc177dfd0203565a7e6d1055ae84a784389e0f
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2022-08-23 19:02:50 -0300

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,16 @@
+from itertools import zip_longest
+a = [3,4,5,6,9]
+b = [7,9,3,2,1,3,0,7]
+
+
+print('-=' * 15)
+print('SOMADOR MAIOR')
+somador_maior = [x + y for x,y in zip_longest(a,b,fillvalue=0)]
+print(somador_maior)
+
+
+print('-=' * 15)
+print('SOMADO MENOR ')
+
+somador_de_A_e_B = [x + y for x, y in zip(a,b)]
+print(somador_de_A_e_B)
