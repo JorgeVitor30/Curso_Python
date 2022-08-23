b2f88efee63dc96e4af5f6d30fdf1216820f6033
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2022-08-23 19:00:49 -0300

    Create ex04.py

---
+++ ex04.py
@@ -0,0 +1,21 @@
+from itertools import count
+nome = []
+media = []
+indice = count()
+
+for x in range(0,2):
+    nome.append(str(input('Digite o seu nome: ')).title())
+    media.append(float(input('Sua média: ')))
+
+lista = list(zip(indice,nome,media))
+    
+    
+a = 'ID'
+b = 'NOME'
+c = 'MÉDIA'
+
+print('-='*15)
+print(f'{a:<10} {b:>7} {c:>11}')
+for ind , nome , media in lista:
+    print(f'{ind:<10} {nome:>8} {media:>8}')
+print('-='*15)  
