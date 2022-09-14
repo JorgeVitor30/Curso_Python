0ba48c68cc199129244fda2c6b5e4367901399ec
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2022-09-14 19:09:25 -0300

    Update ex09.py

--- ex09.py
+++ ex09.py
@@ -13,9 +13,9 @@ class Bola:
         
     def mostraTudo(self):
         print('-=' * 15)
-        print(f'Cor: {self.cor}')
+        print(f'Cor: {self.cor.title()}')
         print(f'Raio : {self.raio} cm')
-        print(f'Material: {self.material}')
+        print(f'Material: {self.material.title()}')
         print('-=' * 15)
         print()
         
@@ -26,12 +26,16 @@ class Bola:
         
 b1 = Bola('Azul',15,'ferro')
 b2 = Bola('Amarelo',30,'papel')
+b3 = Bola('marrom',12,'aluminio')
+
 
 b1.trocaCor('Vermelho')
 
 
 b1.mostraTudo()
 b2.mostraTudo()
+b3.mostraTudo()
+
 
 print(b1.mostrarArea())
 print(b2.mostrarArea())
