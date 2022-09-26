d09a350102763000385be11b8e019eea19af8ca1
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2022-09-26 19:43:49 -0300

    Update and rename posto_gasolina.py to ex16.py

--- ex16.py
+++ ex16.py
@@ -1,18 +1,3 @@
-# Possua uma classe chamada bombaCombustível, com no mínimo esses atributos:
-
-# tipoCombustivel.
-# valorLitro
-# quantidadeCombustivel
-
-# Possua no mínimo esses métodos:
-# abastecerPorValor( ) – método onde é informado o valor a ser abastecido e mostra a quantidade de litros que foi colocada no veículo
-# abastecerPorLitro( ) – método onde é informado a quantidade em litros de combustível e mostra o valor a ser pago pelo cliente.
-# alterarValor( ) – altera o valor do litro do combustível.
-# alterarCombustivel( ) – altera o tipo do combustível.
-# alterarQuantidadeCombustivel( ) – altera a quantidade de combustível restante na bomba.
-
-# OBS: Sempre que acontecer um abastecimento é necessário atualizar a quantidade de combustível total na bomba
-
 class bombaCombustivel:
     def __init__(self, qtdTanque , limiteTanque = 55):
         self.qtdTanque = qtdTanque
