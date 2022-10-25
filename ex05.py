c53fd38cb75d261e09ce6fca17baea9af23896f7
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2022-10-25 19:11:55 -0300

    List Comprehension

--- ex05.py
+++ ex05.py
@@ -9,7 +9,7 @@ somador_maior = [x + y for x,y in zip_longest(a,b,fillvalue=0)]
 print(somador_maior)
 
 
-print('-=' * 15)
+print('-=' * 16)
 print('SOMADO MENOR ')
 
 somador_de_A_e_B = [x + y for x, y in zip(a,b)]
