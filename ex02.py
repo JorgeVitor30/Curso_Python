57d4e23e87125f6f66ca751d00cf5222cbcb2f64
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2022-10-25 19:10:17 -0300

    List Comprehension

--- ex02.py
+++ ex02.py
@@ -1,4 +1,4 @@
-tudo = [1,4,5,6,7,4,3,32,4,4,44,4,4,3,2,5,5,6,7,8,9,'Jorge','Jorge']
-tudo = set(tudo)   
-tudo = list(tudo)
-print(tudo)
+TUDO = [1,4,5,6,7,4,3,32,4,4,44,4,4,3,2,5,5,6,7,8,9,'Jorge','Jorge']
+TUDO = set(TUDO)   
+TUDO = list(TUDO)
+print(TUDO)
